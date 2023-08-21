db8d2c5b4de351dc31be3ee1bb93c6741cb9db7e
nja93 <njanjalchege@gmail.com>
2023-08-21 14:27:20 -0700

    my sql setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,4 @@
+-- Create Database and new user if does not exist, also grant privileges
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
