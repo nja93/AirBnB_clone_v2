cdef21d1260727f35744e721fe9ed2f64a9ce72c
Winfred <winfrednginakilonzo@gmail.com>
2023-08-22 00:23:39 +0300

    set password for hbnb_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- Create database and use and grant all privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
