a1754bb07d75a489b6b3a45d07f299a5a12847dd
Winfred <winfrednginakilonzo@gmail.com>
2023-08-23 21:51:44 +0300

    revoke changes

--- models/city.py
+++ models/city.py
@@ -12,6 +12,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities_relationship")
     places = relationship('Place', cascade='all, delete, delete-orphan',
                           backref='cities')
--- models/state.py
+++ models/state.py
@@ -12,8 +12,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities_relationship = relationship("City", back_populates="state",
-                                       cascade='all,delete-orphan')
+    cities = relationship("City", backref='state', cascade='all, delete, delete-orphan')
 
     @property
     def cities(self):
