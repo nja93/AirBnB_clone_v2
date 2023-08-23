1f1985fb2be901c94b372eeb0ae68f4e1a706bf9
Winfred <winfrednginakilonzo@gmail.com>
2023-08-23 22:32:55 +0300

    revoke changes

--- models/place.py
+++ models/place.py
@@ -7,16 +7,6 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 import models
 
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
-
-
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
@@ -31,36 +21,4 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
-
-
-    '''check if the environment variable HBNB_TYPE_STORAGE is set to "db"'''
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship('Review', cascade='all, delete, delete-orphan',
-                               backref='place')
-        amenities = relationship('Amenity', secondary=place_amenity,
-                                 viewonly=False,
-                                 back_populates='place_amenities')
-
-    '''if not set to db, defines property reviews'''
-    else:
-        @property
-        def reviews(self):
-            from models import storage
-            rev_instances = storage.all("Review").values()
-            return [review for review in rev_instances
-                    if review.place_id == self.id]
-    
-        @property
-        def amenities(self):
-            from models import storage
-            instance of amenity = storage.all("Amenity").values()
-            return [amenity for amenity in amenity_instances
-                    if amenity.id in self.amenity_ids]
-
-        @amenities.setter
-        def amenities(self, obj):
-            from models.amenity import Amenity
-            if isinstance(obj, Amenity):
-                self.amenity_ids.append(obj.id)
-
     
