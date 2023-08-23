6605cc142450972ae49f788b1bb9a85fd9940473
nja93 <njanjalchege@gmail.com>
2023-08-23 16:23:03 -0700

    question 9

--- models/review.py
+++ models/review.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, ForeignKey
 import models
 
-
-class Review(BaseModel):
-    """ Review class to store review information """ 
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review class to store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),nullable=False)
+    text = Column(String(1024), nullable=False)
--- models/user.py
+++ models/user.py
@@ -12,5 +12,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship(
-        'Place', cascade='all, delete, delete-orphan', backref='user')
+
+    places = relationship('Place', cascade='all, delete, delete-orphan', backref='user')
+    reviews = relationship('Review', cascade='all, delete, delete-orphan', backref='user')
