adc4b1c714097559edffde0b4f4643873bab8a77
Winfred <winfrednginakilonzo@gmail.com>
2023-08-23 15:11:56 +0300

    update place

--- models/city.py
+++ models/city.py
@@ -13,3 +13,5 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     state = relationship("State", back_populates="cities_relationship")
+    places = relationship('Place', cascade='all, delete, delete-orphan',
+                          backref='cities')
