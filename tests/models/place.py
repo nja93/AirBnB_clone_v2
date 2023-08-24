4f2df55f9e47c4a2cd77d412f4793f1f3a9687be
Winfred <winfrednginakilonzo@gmail.com>
2023-08-24 03:33:58 +0300

    revoke changes

---
+++ tests/models/place.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Float, Integer, Table
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+import models
+
+
+class Place(BaseModel, Base):
+    """ A place to stay """
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
