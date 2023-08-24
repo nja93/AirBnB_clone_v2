d60f7ca62a0b3cc9db68c4430d7ce7c01c777737
Winfred <winfrednginakilonzo@gmail.com>
2023-08-24 05:36:41 +0300

    import files

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     '''class amenity'''
     __tablename__ = 'amenities'
     name =  Column(String(128), nullable=False)
