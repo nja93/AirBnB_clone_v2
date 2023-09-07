e24828a8726900e848295bf49fcf042e1ef23431
Winfred <winfrednginakilonzo@gmail.com>
2023-09-07 11:09:47 -0700

    fix pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 
 def do_pack():
-	"""Create a tar gzipped archive of the directory web_static."""
-	now = datetime.now().strftime("%Y%m%d%H%M%S")
-	archive_path = "versions/web_static_{}.tgz".format(now)
-	local("mkdir -p versions")
-	archived = local("tar -cvzf {} web_static".format(archive_path))
-	if archived.return_code != 0:
-		return None
-	else:
-		return archive_path
+    """Create a tar gzipped archive of the directory web_static."""
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+    local("mkdir -p versions")
+    archived = local("tar -cvzf {} web_static".format(archive_path))
+    if archived.return_code != 0:
+        return None
+    else:
+        return archive_path
