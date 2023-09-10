c97232612537f8714acd9035424c9b1e971db7cc
nja93 <njanjalchege@gmail.com>
2023-09-11 02:33:53 +0300

    please work

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,41 @@
 #!/usr/bin/python3
-# Fabfile that distributes an archive to your web servers
-from fabric.api import put, env, run
+# this script that distributes an archive to your web servers
+
+from fabric.api import *
+from datetime import datetime
 import os
 
+env.hosts = ["ubuntu@54.146.84.26", "ubuntu@54.161.251.62"]
+env.user = 'ubuntu'
+# env.key_filename = '~/.ssh/id_rsa'
 
-env.hosts = ["52.91.168.104", "100.24.253.204"]
+def do_pack():
+    #Create a tar gzipped archive of the directory web_static."""
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+    local("mkdir -p versions")
+    archived = local("tar -cvzf {} web_static".format(archive_path))
+    if archived.return_code != 0:
+        return None
+    else:
+        return archive_path
 
 
 def do_deploy(archive_path):
     '''use os module to check for valid file path'''
     if os.path.exists(archive_path):
-        # Remove version
-        filewithextension = archive_path.split("/")[-1]
-        filename = filewithextension.split(".")[0]
-        if put(archive_path, "/tmp/{}".format(filename)).failed is True:
-            return False
-            # If failed is True, means upload was unseccessful
-        if run("rm -rf /data/web_static/releases/{}/".
-                format(filename)).failed is True:
-            # Remove existing web app version
-            return False
-        if run("mkdir -p /data/web_static/releases/{}/".
-                format(filename)).failed is True:
-            # create a new empty directory
-            # if it fails return false
-            return False
-        if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-                format(filewithextension, filename)).failed is True:
-            '''
-            tar decompresses and compresses tape archives files and directories
-            -x: specifies files to extract
-            -z: Specifies archives uses gzip compression
-            -f: name of archive file to extract
-            -C: specifies where files are stored after extraction
-            '''
-            return False
-        if run("rm /tmp/{}".format(filewithextension)).failed is True:
-            return False
-        if run("mv /data/web_static/releases/{}/web_static/*"
-                "/data/web_static/releases/{}/".
-                format(filename, filename)).failed is True:
-            return False
-            # move the files to right directory
-        if run("rm -rf /data/web_static/releases/{}/web_static/".
-                format(filename, filename)).failed is True:
-            return False
-            # Remove the directory
-        if run("rm -rf /data/web_static/current").failed is True:
-            # Remove current symbolic link
-            return False
-        if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-                format(filename)).failed is True:
-            # create a new symbolic link with the new version
-            return False
-        return True
-        # if all steps are executed sucessfully return True
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
 
-    else:
-        return False
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+        return True
+    return False
